EEF motion prediction fixed. fingertip_replay had issues with predicting endeffector position hence the fingertip poses. Problems were with intrinsic matrix and the rotation matrices being transposed.

envs/dexterous_env/fingertip_motion_env.py
@@ -38,7 +38,7 @@ class FingertipMotionEnv(DexterousSimulationEnv):
         table_asset_file = "allegro_hand_description/urdf/table.urdf"
 
         asset_options = gymapi.AssetOptions()
-        asset_options.fix_base_link = True
+        asset_options.fix_base_link = False
         asset_options.flip_visual_attachments =  False 
         asset_options.use_mesh_materials = True
         asset_options.disable_gravity = True
@@ -63,27 +63,6 @@ class FingertipMotionEnv(DexterousSimulationEnv):
         self.table_pose.p = gymapi.Vec3(0.0, 0.0, 0.0)
         self.table_pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
 
-    def _turn_frame_to_homo_mat(self, rvec, tvec):
-
-        homo_mat = np.zeros((4, 4))
-        homo_mat[:3, :3] = np.transpose(rvec)
-        homo_mat[:3, 3] = tvec
-        homo_mat[3, 3] = 1
-
-        return homo_mat
-    
-    def _turn_quat_to_rot(self, quat):
-        rot = Rotation.from_quat(quat) 
-        return rot.as_matrix()
-    
-    def _get_endeff_pose(self):
-        position = self.get_endeff_position()
-        tvec = position[:3]
-        rvec = self._turn_quat_to_rot(position[3:])
-        homo_mat = self._turn_frame_to_homo_mat(rvec=rvec, tvec=tvec)
-
-        return homo_mat
-
     def create_camera_sensors(self): 
         print('  ** Creating camera sensors **')
         camera_props = gymapi.CameraProperties() 
@@ -97,7 +76,7 @@ class FingertipMotionEnv(DexterousSimulationEnv):
         print('  Created camera sensor')
         
         # Actually set the camera position
-        camera_position = gymapi.Vec3(0.18, 2.3, 0.0)
+        camera_position = gymapi.Vec3(1.2, 1.8, 0.0)
         camera_target = gymapi.Vec3(0.2, 1.5, 0.0)
         self.gym.set_camera_location(self.camera_handle, self.env, camera_position, camera_target)
         print('  Set camera location')
@@ -105,21 +84,16 @@ class FingertipMotionEnv(DexterousSimulationEnv):
         print('  Started access to image tensors')
 
         # Get the camera pose - view matrix and invert it to transform points from camera to world space
-        self.camera_pose = np.transpose(
-            np.linalg.inv(np.matrix(self.gym.get_camera_view_matrix(self.sim, self.env, self.camera_handle))))
-        print('camera pose: {}'.format(self.camera_pose))
+        self.camera_pose = np.linalg.pinv(np.transpose(np.matrix(self.gym.get_camera_view_matrix(self.sim, self.env, self.camera_handle))))
 
         # Get the intrinsics matrix of the camera
         self.intrinsic_matrix = self.compute_camera_intrinsics_matrix(
             image_width = camera_props.width, 
             image_heigth = camera_props.height, 
-            horizontal_fov = camera_props.horizontal_fov,
-            device = 'cpu'
+            horizontal_fov = camera_props.horizontal_fov
         )
 
-        print('intrinsic_matrix: {}'.format(self.intrinsic_matrix))
-
-    def compute_camera_intrinsics_matrix(self, image_width, image_heigth, horizontal_fov, device):
+    def compute_camera_intrinsics_matrix(self, image_width, image_heigth, horizontal_fov):
         vertical_fov = (image_heigth / image_width * horizontal_fov) * np.pi / 180
         horizontal_fov *= np.pi / 180
 
@@ -127,7 +101,7 @@ class FingertipMotionEnv(DexterousSimulationEnv):
         f_y = (image_heigth / 2.0) / np.tan(vertical_fov / 2.0)
 
         K = np.asarray([
-            [f_x, 0.0, image_width / 2.0], 
+            [-f_x, 0.0, image_width / 2.0], 
             [0.0, f_y, image_heigth / 2.0],
             [0.0, 0.0, 1.0]], dtype=np.float32)
 
@@ -156,14 +130,56 @@ class FingertipMotionEnv(DexterousSimulationEnv):
         obs = {}
         obs['pixels'] = self.compute_observation(obs_type = 'image') 
         obs['features'] = self.compute_observation(obs_type = 'position')
-        # print('AFTER RESET OBS: {} | {}'.format(obs['pixels'].shape, obs['features'].shape))
-        print('Features received: {}'.format(obs['features']))
         
         return obs
 
-    def calculate_fingertip_positions(self, features):
-        # print('features.shape in fingertip_pos calculation: {}'.format(features.shape))
+    def _turn_frame_to_homo_mat(self, rvec, tvec):
 
+        homo_mat = np.zeros((4, 4))
+        homo_mat[:3, :3] = rvec
+        homo_mat[:3, 3] = tvec
+        homo_mat[3, 3] = 1
+
+        return homo_mat
+    
+    def _turn_quat_to_rot(self, quat):
+        rot = Rotation.from_quat(quat) 
+        return rot.as_matrix()
+    
+    def _get_home_endeff_pose(self, relative_to_camera=True):
+        # We are going to calculate the desired pose 
+        # wrt the beginning pose
+        # This is the home endeff position
+        home_tvec = self.endeff_home_state[:3]
+        home_rvec = self._turn_quat_to_rot(self.endeff_home_state[3:])
+        H_EH_O = self._turn_frame_to_homo_mat(rvec = home_rvec, tvec = home_tvec) # Home position of end effector
+
+        if relative_to_camera:
+            # Calculate the current endeff pose wrt the camera
+            H_C_O = self.camera_pose
+            H_EH_C = np.linalg.pinv(H_C_O) @ H_EH_O
+            return H_EH_C
+        else:
+            return H_EH_O
+    
+    def _get_current_endeff_pose(self, relative_to_camera=True):
+        position = self.get_endeff_position()
+
+        # Turn position to homo matrix
+        tvec = position[:3]
+        rvec = self._turn_quat_to_rot(position[3:])
+        H_ET_O = self._turn_frame_to_homo_mat(rvec= rvec, tvec=tvec)
+
+        if relative_to_camera:
+            # Calculate the current endeff pose wrt the camera
+            H_C_O = self.camera_pose
+            H_ET_C = np.linalg.pinv(H_C_O) @ H_ET_O
+            return H_ET_C
+        else: 
+            return H_ET_O
+
+
+    def calculate_fingertip_positions(self, features):
         fingertip_poses = []
         # Calculate translational and rotational fingertip positions
         for i, finger_type in enumerate(['index', 'middle', 'ring', 'thumb']):
@@ -172,29 +188,53 @@ class FingertipMotionEnv(DexterousSimulationEnv):
             )
 
             # Stack tvec and rvec
-            fingertip_pose = self._turn_frame_to_homo_mat(rvec=finger_rvec, tvec=finger_tvec)
+            fingertip_pose = self._turn_frame_to_homo_mat(
+                rvec=finger_rvec,
+                tvec=finger_tvec)
             # print('fingertip_pose.shape: {}'.format(fingertip_pose))
             fingertip_poses.append(fingertip_pose)
 
         fingertip_poses = np.stack(fingertip_poses, axis=0)
-        # print('fingertip_poses.shape: {}'.format(fingertip_poses.shape))
 
         return fingertip_poses
+    
+    def calculate_fingertip_positions_wrt_world(self, features, endeff_pose='home_pose'):
+        # If from_data is set to true endeff pose will be taken from the data collected
+        if endeff_pose == 'home_pose': 
+            H_E_O = self._get_home_endeff_pose(
+                relative_to_camera = False
+            )
+        elif endeff_pose == 'current_pose':
+            H_E_O = self._get_current_endeff_pose(
+                relative_to_camera = False
+            )
 
-    def get_projected_fingertip_positions(self, features):
+        fingertip_poses = self.calculate_fingertip_positions(features = features)
+        fingertip_poses_wrt_world = []
+        for H_F_E in fingertip_poses: # Homo to take fingertip pose frame to the end effector frame
+            H_F_O = H_E_O @ H_F_E  # Homo to take fingertip pose frame to the origin 
+            fingertip_poses_wrt_world.append(H_F_O)
+
+        fingertip_poses_wrt_world = np.stack(fingertip_poses_wrt_world, axis=0)
+        return fingertip_poses_wrt_world
+
+    def get_projected_fingertip_positions(self, features, endeff_pose='home_pose'):
+
         # Get the fingertip positions in 3d
         fingertips_3d = self.calculate_fingertip_positions(features)
 
-        H_C_O = self.camera_pose # Homo to take camera frame to origin
-        H_E_O = self._get_endeff_pose() # Homo to take end effector pose frame to origin
+        # Calculate the camera pose wrt the camera
+        H_E_C = self._get_current_endeff_pose(
+            relative_to_camera = True
+        )        
 
         fingertips_2d = []
         # Get the distance of the fingertips to the camera 
         for finger_id in range(len(fingertips_3d)):
-            H_F_E = fingertips_3d[finger_id] # Homo to take finger frame to origin
+            H_F_E = fingertips_3d[finger_id] # Homo to take finger frame to endeffector
 
             # We want the homo to take finger frame to camera frame
-            H_F_C = np.linalg.inv(H_C_O) @ H_E_O @ H_F_E 
+            H_F_C = H_E_C @ H_F_E
 
             # Extract the tvec and rvecs on this homo
             rvec, tvec = H_F_C[:3, :3], H_F_C[:3, 3]
@@ -205,6 +245,22 @@ class FingertipMotionEnv(DexterousSimulationEnv):
 
         fingertips_2d = np.stack(fingertips_2d, axis=0)
         return fingertips_2d
+    
+    def get_projected_endeffector_position(self, endeff_pose='home_pose'):
+
+        if endeff_pose == 'home_pose': 
+            H_E_C = self._get_home_endeff_pose(
+                relative_to_camera = True
+            )
+        elif endeff_pose == 'current_pose':
+            H_E_C = self._get_current_endeff_pose(
+                relative_to_camera = True
+            )
+
+        rvec, tvec = H_E_C[:3, :3], H_E_C[:3, 3]
+        eef_2d = self.project_axes(rvec, tvec, self.intrinsic_matrix)
+
+        return [eef_2d]
             
     def project_axes(self, rvec, tvec, intrinsic_matrix, scale=0.05, dist=None):
         """
@@ -220,10 +276,5 @@ class FingertipMotionEnv(DexterousSimulationEnv):
         # img = img.astype(np.float32)
         dist = np.zeros(4, dtype=float) if dist is None else dist
         points = scale * np.float32([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]]).reshape(-1, 3)
-        # print('intrinsic_matrix: {}'.format(intrinsic_matrix))
         axis_points, _ = cv2.projectPoints(points, rvec, tvec, intrinsic_matrix, dist)
-        # img = cv2.line(img, tuple(axis_points[3].ravel()), tuple(axis_points[0].ravel()), (255, 0, 0), 3)
-        # img = cv2.line(img, tuple(axis_points[3].ravel()), tuple(axis_points[1].ravel()), (0, 255, 0), 3)
-        # img = cv2.line(img, tuple(axis_points[3].ravel()), tuple(axis_points[2].ravel()), (0, 0, 255), 3)
-        # return img
         return axis_points

third_person_man/testing/fingertip_camera_replay.py
@@ -14,47 +14,63 @@ from tqdm import tqdm
 from .trajectory_replay import TrajectoryReplay
 
 class FingertipReplay(TrajectoryReplay):
-    def __init__(self, data_path, demo_num, representations, env_cfg): 
-        # Initialize the environment required
-        self.env = hydra.utils.instantiate(env_cfg) 
-
-        # Load the data
-        from third_person_man.utils import load_data # Will give an error for the torch imports 
-        roots = glob.glob(f'{data_path}/demonstration_*')
-        self.data = load_data(roots=roots, demos_to_use=[demo_num], representations=representations)
-        self.state_id = 0
-
-        # Test if the initialization is proper
-        obs = self.env.reset()
-
-        # Initialize the video recorder
-        from third_person_man.utils import VideoRecorder
-        work_dir = os.path.dirname(__file__)
-        self.video_recorder = VideoRecorder(
-            save_dir = Path(work_dir) / 'outs/fingertip_replay',
+    def __init__(self, data_path, demo_num, representations, env_cfg, module_name='fingertip_replay'): 
+        super().__init__(
+            data_path = data_path,
+            demo_num = demo_num,
+            representations = representations,
+            env_cfg = env_cfg,
+            module_name = module_name
         )
-
-        plt.imshow(np.transpose(obs['pixels'], (1,2,0)))
-        plt.savefig(f'{work_dir}/outs/fingertip_replay/reset.jpg')
 
 
     def save_timestep(self, state_id): 
         # Get the action from the data
         demo_id, action_id = self.data['hand_actions']['indices'][state_id]
         hand_action = self.data['hand_actions']['values'][demo_id][action_id]
-        action = np.concatenate([hand_action, np.zeros(7)], axis=0)
+        
+        # Just as a test we are adding mock actions
+        state_div = state_id / len(self.data['hand_actions']['indices'])
+        buffer = 0.5
+        if state_div < 0.33:
+            res = state_div / 0.33 * buffer
+            res_x =  res if state_div < 0.165 else buffer-res
+            res_y, res_z = 0, 0
+        elif state_div < 0.66:
+            res = (state_div-0.33) / 0.33 * buffer
+            res_y = res if state_div < (0.33+0.165) else buffer-res
+            res_x, res_z = 0, 0
+        else: 
+            res = (state_div-0.66) / 0.33 * buffer
+            res_z = res if state_div < (0.66+0.165) else buffer-res
+            res_x, res_y = 0, 0
+        arm_action = np.asarray(
+            [0.2-res_x, 1.5+res_y, 0.0-res_z, 0, 0, 0.7071068, 0.7071068 ]
+        )
+        residual_action = np.array([
+            [-res_x],
+            [res_y],
+            [-res_z]
+        ])
+        print('residual_action: {}'.format(residual_action))
+        action = np.concatenate([hand_action, arm_action], axis=0)
+        # action = np.concatenate([hand_action, np.zeros(7)], axis=0)
 
         # Apply to the environment
         obs, _, _, _ = self.env.step(action)
-        # print(f'in save_timestep: obs.shape: {obs["pixels"].shape}')
-        # print('features: {}'.format(obs["features"]))
-
-        # Get the transformed fingertip positions
-        fingertips_2d = self.env.get_projected_fingertip_positions(obs['features'])
+        self.record_fingertip_poses(obs=obs)
         
-        work_dir = os.path.dirname(__file__)
-        plt.imshow(np.transpose(obs['pixels'], (1,2,0)))
-        plt.savefig(f'{work_dir}/outs/fingertip_replay/pre_fingertips_obs.jpg')
+
+    def record_fingertip_poses(self, obs):
+        # Get the transformed fingertip positions
+        fingertips_2d = self.env.get_projected_fingertip_positions(
+            obs['features'],
+            endeff_pose = 'current_pose'
+        )
+        
+        # work_dir = os.path.dirname(__file__)
+        # plt.imshow(np.transpose(obs['pixels'], (1,2,0)))
+        # plt.savefig(f'{work_dir}/outs/fingertip_replay/pre_fingertips_obs.jpg')
 
         # Plot the fingertips on top of the image
         # img = np.transpose(np.float32(obs['pixels']) / 255., (1,2,0))
@@ -62,44 +78,58 @@ class FingertipReplay(TrajectoryReplay):
             np.ones(np.transpose(obs['pixels'], (1,2,0)).shape, np.uint8),
             np.transpose(obs['pixels'], (1,2,0))
         )
-        img = self.plot_fingertips(fingertips_2d=fingertips_2d, img=img)
+        img = self.plot_axes(axes=fingertips_2d, img=img)
 
-        work_dir = os.path.dirname(__file__)
-        plt.imshow(img)
-        plt.savefig(f'{work_dir}/outs/fingertip_replay/post_fingertips_obs.jpg')
+        # work_dir = os.path.dirname(__file__)
+        # plt.imshow(img)
+        # plt.savefig(f'{work_dir}/outs/fingertip_replay/post_fingertips_obs.jpg')
+
+        # Plot the end effector position
+        eef_2d = self.env.get_projected_endeffector_position(
+            endeff_pose = 'current_pose'
+        )
+        img = self.plot_axes(axes=eef_2d, img=img, color_set=2)
+
+        home_eef_2d = self.env.get_projected_endeffector_position(
+            endeff_pose = 'home_pose'
+        )
+        img = self.plot_axes(axes=home_eef_2d, img=img, color_set=2)
 
         # Get the image and write to the video recorder
         self.video_recorder.record(
             obs = np.transpose(img, (2,0,1))
         )
+    
+    def plot_axes(self, axes, img, color_set=1):
+        for axis in axes:
+            axis = axis.astype(int)
+            if color_set == 1:
+                img = cv2.line(img, tuple(axis[3].ravel()), tuple(axis[0].ravel()), (255, 0, 0), 3)
+                img = cv2.line(img, tuple(axis[3].ravel()), tuple(axis[1].ravel()), (0, 255, 0), 3)
+                img = cv2.line(img, tuple(axis[3].ravel()), tuple(axis[2].ravel()), (0, 0, 255), 3)
+        
+            elif color_set == 2:
 
-    def plot_fingertips(self, fingertips_2d, img):
-        # print(img)
-        # print('img.shape: {}'.format(img.shape))
-        # print(img)
-        for fingertip_pos in fingertips_2d:
-            fingertip_pos = fingertip_pos.astype(int)
-            img = cv2.line(img, tuple(fingertip_pos[3].ravel()), tuple(fingertip_pos[0].ravel()), (255, 0, 0), 3)
-            img = cv2.line(img, tuple(fingertip_pos[3].ravel()), tuple(fingertip_pos[1].ravel()), (0, 255, 0), 3)
-            img = cv2.line(img, tuple(fingertip_pos[3].ravel()), tuple(fingertip_pos[2].ravel()), (0, 0, 255), 3)
+                img = cv2.line(img, tuple(axis[3].ravel()), tuple(axis[0].ravel()), (255, 165, 0), 3) # Orange
+                img = cv2.line(img, tuple(axis[3].ravel()), tuple(axis[1].ravel()), (128, 128, 0), 3) # Green
+                img = cv2.line(img, tuple(axis[3].ravel()), tuple(axis[2].ravel()), (138, 43, 226), 3) # Purple
+        
+
         return img
 
-
     def save_trajectory(self, title='fingertip_replay.mp4'):
-
         obs = self.env.reset()
-        print('obs.shape: {}'.format(obs["pixels"].shape))
-        print('obs.features after resetting: {}'.format(obs["features"]))
         self.video_recorder.init(obs = obs['pixels'])
 
         pbar = tqdm(total=len(self.data['hand_actions']['indices']))
 
         for state_id in range(len(self.data['hand_actions']['indices'])):
-            # print(f'State ID: {state_id}')
             self.save_timestep(state_id)
-
             pbar.update(1)
             pbar.set_description(f'State ID: {state_id}')
+
+            # if state_id > 5: 
+            #     break
 
         self.video_recorder.save(title)
         pbar.close()

third_person_man/testing/trajectory_replay.py
@@ -7,10 +7,12 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 
+from tqdm import tqdm
+
 from pathlib import Path
 
 class TrajectoryReplay:
-    def __init__(self, data_path, demo_num, representations, env_cfg):
+    def __init__(self, data_path, demo_num, representations, env_cfg, module_name='trajectory_replay'):
 
         # Initialize the environment required
         self.env = hydra.utils.instantiate(env_cfg) 
@@ -21,17 +23,19 @@ class TrajectoryReplay:
         self.data = load_data(roots=roots, demos_to_use=[demo_num], representations=representations)
         self.state_id = 0
 
-        # Test if the initialization is proper
         obs = self.env.reset()
-        plt.imshow(np.transpose(obs['pixels'], (1,2,0)))
-        work_dir = os.path.dirname(__file__)
-        plt.savefig(f'{work_dir}/outs/trajectory_replay/reset_obs_small.jpg')
-
         # Initialize the video recorder
         from third_person_man.utils import VideoRecorder
+        work_dir = os.path.dirname(__file__)
         self.video_recorder = VideoRecorder(
-            save_dir = Path(work_dir) / 'outs/trajectory_replay',
+            save_dir = Path(work_dir) / f'outs/{module_name}',
+            fps = 20
         )
+
+        # Test if the initialization is proper
+        plt.imshow(np.transpose(obs['pixels'], (1,2,0)))
+        plt.savefig(f'{work_dir}/outs/{module_name}/env_reset.jpg')
+
 
     def save_timestep(self, state_id): 
         # Get the action from the data
@@ -51,15 +55,17 @@ class TrajectoryReplay:
 
     def save_trajectory(self, title='cube_flipping_trajectory_endeff.mp4'):
         obs = self.env.reset()
-        print('obs.shape: {}'.format(obs["pixels"].shape))
-        print('obs.features after resetting: {}'.format(obs["features"]))
         self.video_recorder.init(obs = obs['pixels'])
 
+        pbar = tqdm(total=len(self.data['hand_actions']['indices']))
+
         for state_id in range(len(self.data['hand_actions']['indices'])):
-            print(f'State ID: {state_id}')
             self.save_timestep(state_id)
+            pbar.update(1)
+            pbar.set_description(f'State ID: {state_id}')
 
         self.video_recorder.save(title)
+        pbar.close()
 
 
 @hydra.main(version_base=None, config_path='../../configs', config_name='testing')